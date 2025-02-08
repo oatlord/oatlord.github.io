166c043347aa89d7957a51e4df59d84b4aa50386
Ren Tamdang <tamdangren@gmail.com>
2025-02-08 23:14:10 +0800

    Exercise 4

---
+++ M2/Tamdang_Exercise_4/script.js
@@ -0,0 +1,19 @@
+// Replace with your WeatherAPI key
+const apiKey = '57d6ddd9a246cb8dfadbdf129d8b17c4';
+// Change this to the city you want to search
+const city = 'Davao'; 
+
+function callAPI() {
+    const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        document.querySelector("#weather_city").textContent = data.location.name || 'N/A';
+        document.querySelector("#weather_temperature").textContent = `${data.current.temp_c}°C` || 'N/A';
+        document.querySelector("#weather_condition").textContent = data.current.condition.text || 'N/A';
+    });
+}
+
